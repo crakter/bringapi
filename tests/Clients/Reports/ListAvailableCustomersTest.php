b3760d4662e9aa54ed6ecfb341f029a49c1d609f
Martin Madsen <crakter@gmail.com>
2017-07-07 11:23:27 +0200

    Improved testcoverage

---
+++ tests/Clients/Reports/ListAvailableCustomersTest.php
@@ -0,0 +1,66 @@
+<?php
+
+/*
+ * This file is part of the BringApi package.
+ *
+ * (c) Martin Madsen <crakter@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Crakter\BringApi\Clients;
+
+use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Client;
+use Crakter\BringApi\Exception\BringClientException;
+use Crakter\BringApi\DefaultData\ReturnFileTypes;
+
+class ListAvailableCustomersTest extends TestCase
+{
+    private $class;
+    private $json;
+
+    public function setUp()
+    {
+        $this->class = (new Reports\ListAvailableCustomers);
+        $this->json = file_get_contents(dirname(dirname(__DIR__)).'/Data/ListAvailableCustomersResponse.json');
+        $this->class->setResponse(new Response(200, ['X-Foo' => 'Bar'], $this->json));
+    }
+
+    public function testGetAvailableCustomers()
+    {
+        $this->assertSame([['id' => "PARCELS_NORWAY-00012341234", 'name' => "TEST CUSTOMER",'fullName' => "TEST CUSTOMER (00012341234)",'reports' => "https://www.mybring.com/reports/api/generate/PARCELS_NORWAY-00012341234/"]], $this->class->getAvailableCustomers());
+    }
+
+    public function testProcessClientUrlVariables()
+    {
+        $this->assertInstanceOf(ClientsInterface::class, $this->class->processClientUrlVariables());
+    }
+
+    public function testCheckErrorsException()
+    {
+        $this->expectException(BringClientException::class);
+        $this->assertInstanceOf(ClientsInterface::class, $this->class->setResponse(new Response(200, ['X-Foo' => 'Bar'], 'test')));
+        $this->class->checkErrors();
+    }
+
+    public function testCheckErrors()
+    {
+        $this->assertInstanceOf(ClientsInterface::class, $this->class->checkErrors());
+    }
+
+    public function testSend()
+    {
+        $array = $this->class->toArray();
+        $mock = new MockHandler([$this->class->getResponse()]);
+        $handler = HandlerStack::create($mock);
+        $client = new Client(['handler' => $handler]);
+        $this->assertInstanceOf(ClientsInterface::class, $this->class->setClient($client));
+        $this->assertInstanceOf(ClientsInterface::class, $this->class->send());
+        $this->assertEquals($array, $this->class->toArray());
+    }
+}
